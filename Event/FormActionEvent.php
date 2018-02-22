8c35170d84d6d50feb040a7850dd94d0ad0256d5
turchakAlex <alexander.turchak@requestum.com>
2018-02-22 16:16:18 +0200

    Fix event dispatch for before delete action

--- Event/FormActionEvent.php
+++ Event/FormActionEvent.php
@@ -7,27 +7,13 @@ use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 
-class FormActionEvent extends Event
+class FormActionEvent extends EntityActionEvent
 {
-    /**
-     * @var Request
-     */
-    protected $request;
-
-    /**
-     * @var
-     */
-    protected $subject;
 
     /**
      * @var Form
      */
     protected $form;
-
-    /**
-     * @var Registry
-     */
-    protected $doctrineRegistry;
 
     /**
      * FormActionEvent constructor.
@@ -38,48 +24,9 @@ class FormActionEvent extends Event
      */
     public function __construct(Request $request, $subject, Form $form, Registry $doctrineRegistry)
     {
-        $this->subject = $subject;
-        $this->request = $request;
+        parent::__construct($request, $subject, $doctrineRegistry);
+
         $this->form = $form;
-        $this->doctrineRegistry = $doctrineRegistry;
-    }
-
-    /**
-     * @param Request $request
-     * @return $this
-     */
-    public function setRequest($request)
-    {
-        $this->request = $request;
-        
-        return $this;
-    }
-
-    /**
-     * @return Request
-     */
-    public function getRequest()
-    {
-        return $this->request;
-    }
-
-    /**
-     * @param $subject
-     * @return $this
-     */
-    public function setSubject($subject)
-    {
-        $this->subject = $subject;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSubject()
-    {
-        return $this->subject;
     }
 
     /**
@@ -99,24 +46,5 @@ class FormActionEvent extends Event
     public function getForm()
     {
         return $this->form;
-    }
-
-    /**
-     * @param Registry $doctrineRegistry
-     * @return $this
-     */
-    public function setDoctrineRegistry($doctrineRegistry)
-    {
-        $this->doctrineRegistry = $doctrineRegistry;
-
-        return $this;
-    }
-
-    /**
-     * @return Registry
-     */
-    public function getDoctrineRegistry()
-    {
-        return $this->doctrineRegistry;
     }
 }
