d68add88a2102eb71243780092907f1319ede345
turchakAlex <alexander.turchak@requestum.com>
2018-09-14 18:37:47 +0300

    Add  Access annotation for hiding property when normalization object for user who does not own object.

--- Serializer/Normalizer/ObjectNormalizer.php
+++ Serializer/Normalizer/ObjectNormalizer.php
@@ -3,8 +3,11 @@
 namespace Requestum\ApiBundle\Serializer\Normalizer;
 
 use Requestum\ApiBundle\Rest\ReferenceWrapper;
+use Requestum\ApiBundle\Rest\Metadata\Access;
 use Doctrine\ORM\Proxy\Proxy;
+use Requestum\ApiBundle\Rest\ResourceMetadataFactory;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer as BaseObjectNormalizer;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
 /**
  * Class ObjectNormalizer
@@ -22,6 +25,16 @@ class ObjectNormalizer extends BaseObjectNormalizer
     protected $attributeExtractionStrategy;
 
     /**
+     * @var AuthorizationChecker
+     */
+    protected $authorizationChecker;
+
+    /**
+     * @var ResourceMetadataFactory
+     */
+    protected $reader;
+
+    /**
      * @param AttributeExtractionStrategy $attributeExtractionStrategy
      *
      * @return $this
@@ -31,6 +44,23 @@ class ObjectNormalizer extends BaseObjectNormalizer
         $this->attributeExtractionStrategy = $attributeExtractionStrategy;
 
         return $this;
+    }
+
+    /**
+     * @param AuthorizationChecker $authorizationChecker
+     */
+    public function setAuthorizationChecker(AuthorizationChecker $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+
+    /**
+     * @param ResourceMetadataFactory $reader
+     */
+    public function setAnnotationReader(ResourceMetadataFactory $reader)
+    {
+        $this->reader = $reader;
     }
 
     /**
@@ -47,6 +77,27 @@ class ObjectNormalizer extends BaseObjectNormalizer
     }
 
     /**
+     * @param object|string $classOrObject
+     * @param string        $attribute
+     * @param null          $format
+     * @param array         $context
+     *
+     * @return bool
+     */
+    protected function isAllowedAttribute($classOrObject, $attribute, $format = null, array $context = array())
+    {
+        $access = $this->reader->getPropertyMetadata($classOrObject, $attribute, Access::class);
+
+        if ($access instanceof Access && ($attributes = $access->value)) {
+            $attributes = is_array($attributes) ? $attributes : [$attributes];
+
+            return $this->authorizationChecker->isGranted($attributes, $context['object']);
+        }
+
+        return parent::isAllowedAttribute($classOrObject, $attribute, $format, $context);
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function normalize($object, $format = null, array $context = array())
@@ -54,6 +105,8 @@ class ObjectNormalizer extends BaseObjectNormalizer
         if ($object instanceof Proxy) {
             $object->__load();
         }
+
+        $context['object'] = $object;
 
         $current = null;
         if ($object instanceof ReferenceWrapper) {
