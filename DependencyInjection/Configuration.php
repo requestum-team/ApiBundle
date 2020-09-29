d5567cdd11e1dec98666555626e66a14d8c065b0
Alexander Andrienko <alexander.andrienko@requestum.com>
2020-09-29 22:23:42 +0300

    fix dependency injection Configuration.php.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('requestum_api_bundle');
-        $rootNode = $treeBuilder->root('core');
+        $treeBuilder = new TreeBuilder('core');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
